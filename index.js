881c2057a36ae9554c85863874cc1a25f403857a
kshitijarora04 <79055679+kshitijarora04@users.noreply.github.com>
2025-05-30 01:39:14 +0530

    initial commit

--- index.js
+++ index.js
@@ -10,8 +10,7 @@ import connectDB from "./utils/db.js";
 import cookieParser from "cookie-parser";
 
 // import all routes
-import userRoutes from "../Building Full Stack Project/routes/user.routes.js"
-
+import userRoutes from "../Building Full Stack Project/routes/user.routes.js";
 
 // cors is used so that my frontend can only send a request to my backend
 dotenv.config();
@@ -22,18 +21,21 @@ const app = express();
 app.use(cookieParser());
 
 // we can write configurations inside cors
-app.use(cors({
-    origin: process.env.BASE_URL,
-    methods: ['GET', 'POST', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-})
+app.use(
+  cors({
+    // origin should match the frontend url
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "Accept"],
+    exposedHeaders: ["Set-Cookie", "*"],
+  })
 );
 
 // to send json from frontend to backend
-app.use(express.json())
+app.use(express.json());
 
 // to support url-encoding such as %20 = space
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
 const port = process.env.PORT || 4000;
 
@@ -41,25 +43,25 @@ const port = process.env.PORT || 4000;
 // this will run on request
 // Routes always start with /
 
-app.get('/', (req, res) => {
-    res.send('cohort!!');
-})
+app.get("/", (req, res) => {
+  res.send("cohort!!");
+});
 
-app.get('/kshitij', (req, res) => {
-    res.send('kshitij!!');
-})
+app.get("/kshitij", (req, res) => {
+  res.send("kshitij!!");
+});
 
 // callbacks are also known as controllers
 app.get("/hitesh", (req, res) => {
-    res.send("Hitesh")
-})
+  res.send("Hitesh");
+});
 
 // connecting to DB
 connectDB();
 
 // user routes
-app.use("/api/v1/users", userRoutes)
+app.use("/api/v1/users", userRoutes);
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
